d5c93faf4e065b05bccdd09b7c7534a8246be7b3
ElifKumbetli <elifkumbetli@gmail.com>
2025-09-27 16:31:47 +0300

    SweetAlert2 ve Axios entegrasyonu yapıldı

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,35 @@
-import './bootstrap';
+import { Loader } from "./loader";
+import { Login } from "./Pages/Login";
+
+document.addEventListener("DOMContentLoaded", () => {
+    // Login alert çalışsın
+    new Login();
+
+    // App başlat
+    window.app = new App();
+});
+
+export class App {
+    constructor() {
+        this.loader = new Loader();
+        setTimeout(() => {
+            this.loader.load(this.geturl());
+        }, 50);
+
+        this.events();
+    }
+
+    events() {}
+
+    geturl() {
+        let url = window.location.href;
+        url = url
+            .replaceAll("#", "")
+            .replaceAll("!", "")
+            .split(window.location.hostname);
+        url = url[1].split("?");
+        url = url[0].split("/");
+        if (url.length > 1) url = url.splice(1, url.length - 1);
+        return url;
+    }
+}
